0b9addb5876c5e61d6c0e101bd91cb84a4fa237b
Eloy Chang <eloy.chang@location-world.com>
2020-04-10 19:20:55 -0300

    Agregado grafico de importancia de variables tomando solo variables creadas por el equipo

--- Grafico_importancia_variables.R
+++ Grafico_importancia_variables.R
@@ -1,6 +1,9 @@
 
 library(plotly)
 
+
+# para maxDepthArbol (variables originales + totalSpend y winingRate)
+
 load("Arbol.RData")
 
 df<- data.frame(
@@ -17,4 +20,27 @@ plot_ly(df, type = "bar", orientation = "h",x = ~importance, y = ~reorder(variab
   layout(title = "Importancia de variables", xaxis = list(title = "Importancia"),
          yaxis = list(title = ""))
 
+# para maxdepthTree
 
+load("Tree.RData")
+
+df<- data.frame(
+  variable = names(maxDepthTree$variable.importance),
+  importance = as.numeric(maxDepthTree$variable.importance),
+  text = c("Suma de todas las partidas ganadas entre las partidas jugadas",
+           "Suma de todo lo gastado en el juego (hard y soft negative)",
+           "Suma de todas las cartas compradas",
+           "Tasa de victorias durante el primer día",
+           "Tasa de victorias durante el segundo día",
+           "Tasa de victorias durante el tercer día",
+           "Tasa de victorias durante el cuarto día",
+           "Si  hubo al menos un día en el que no se iniciara una partida",
+           "Si el usuario es acreedor",
+           "Suma de cambios de arena")
+)
+df<- df[order(df$importance, decreasing = TRUE),]
+
+plot_ly(df, type = "bar", orientation = "h",x = ~importance, y = ~reorder(variable,importance),
+        text = ~paste(variable,": ",round(importance),"\n",text,sep = ""),hoverinfo = "text") %>%
+  layout(title = "Importancia de variables", xaxis = list(title = "Importancia"),
+         yaxis = list(title = ""))
